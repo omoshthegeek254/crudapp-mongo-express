1c6cedd52e2de982eaddbf3d482f7023682effb7
Elvin <omoshthegeek@gmail.com>
2022-02-27 09:10:30 +0300

    added the test page for the express

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const app = express();
 app.listen(3000, function() {
     console.log('listening on 3000')
   })
+
+  /*app.get('/', function(req, res) {
+    res.send('Hello World')
+  }) */
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+  })
