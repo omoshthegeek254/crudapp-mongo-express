bb852d0951e335ef972cab8d012835b164faf26c
Elvin <omoshthegeek@gmail.com>
2022-02-27 15:26:18 +0300

    added git ignore file

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const bodyParser= require('body-parser')
+const app = express()
+const connectionString= "mongodb+srv://omoshthegeek:<Qwerty2030>@cluster0.lo8h3.mongodb.net/crudapp?retryWrites=true&w=majority"
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: true })) 
+
+
 
 app.listen(3000, function() {
     console.log('listening on 3000')
@@ -10,6 +18,22 @@ app.listen(3000, function() {
   }) */
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
-    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+    // Note: __dirname is the .
+  })
+
+  /*handling post
+  app.post('/quotes', (req, res) => {
+    console.log('Hellooooooooooooooooo!')
+  })*/
+
+  //body parser - hanling form data
+  app.post('/quotes', (req, res) => {
+    console.log(req.body)
+  })
+
+  MongoClient.connect(connectionString, {
+    useUnifiedTopology: true
+  }, (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
   })
